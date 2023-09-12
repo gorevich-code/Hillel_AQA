88d921cdbf425c1db9abaf1ea868da23e87606be
Ivan Horevych <gorevich@outlook.com>
2023-09-13 01:16:08 +0300

    _

--- hw_27_pony/repository.py
+++ hw_27_pony/repository.py
@@ -13,13 +13,22 @@ class CustomersRepo:
     def get_by_id(self, id):
         query = select(customer for customer in Customers if customer.CustomerID == id)
         data = query.get()
-        self.model.orders = [x.to_dict() for x in data.Orders]
+        orders = f'No Orders from {data.Name} {data.SecondName}'
+        if data.Orders:
+            orders = [x.to_dict() for x in data.Orders]
 
-        return dict(self.model)
+        return Customers.ObjectModel(
+            name=data.Name,
+            secondname=data.SecondName,
+            phone = data.Phone,
+            address = data.Address,
+            customerid = data.CustomerID,
+            orders = orders
+        )
 
 
 
 cr = CustomersRepo()
-second = cr.get_by_id(2)
+second = cr.get_by_id(4)
 print(second)
 
