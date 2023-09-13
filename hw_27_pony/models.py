8a66760ed3a288797f1bf873604f66e89081a227
Ivan Horevych <gorevich@outlook.com>
2023-09-13 23:22:35 +0300

    _

--- hw_27_pony/models.py
+++ hw_27_pony/models.py
@@ -13,30 +13,6 @@ class Customers(db.Entity):
     CustomerID = PrimaryKey(int, auto=True, column='CustomerID')
     Orders = Set("Orders")
 
-    class CustomerData:
-        def __init__(self, Name, SecondName, Phone, Address, CustomerID, orders):
-            self.name=Name
-            self.secondname=SecondName,
-            self.phone=Phone,
-            self.address=Address,
-            self.customerid=CustomerID,
-            self.orders=orders
-
-        def __repr__(self):
-            print(
-                f'\n\nName : {self.name} '
-                f'\nSecond Name : {self.secondname} '
-                f'\nPhone : {self.phone} '
-                f'\nAddress : {self.address} '
-                f'\nCustomerID : {self.customerid} '
-                f'\nOrders : {self.orders}\n\n'
-            )
-
-
-
-
-
-
 
 class Orders(db.Entity):
     _table_ = "Orders"
